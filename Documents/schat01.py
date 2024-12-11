f77525a3661060ee780949ae9f390d2d43c52069
punn4021 <77558038+punn4021@users.noreply.github.com>
2024-12-11 11:51:27 -0600

    Modified by www.SourceFiles.app

--- Documents/schat01.py
+++
@@ -1,9 +0,0 @@
-from snapchat import *
->>> me = SnapChat("leetraxler4021")
->>> me.check_username()
-'sdushantha is already taken!'
->>> # Since the usename is taken, that means it is a valid username
->>> me.get_snapcode(bitmoji=False, size=500)
-(..., 'PNG', '500x500')
->>> # The dotted part (...) is the raw data of the image
->>> # You use this data to write it into a file or do whatever you like
