9dc25942631f8e81e9d7e8730bff53ace77c8ab2
punn4021 <77558038+punn4021@users.noreply.github.com>
2024-12-11 05:37:26 -0600

    Modified by www.SourceFiles.app

--- Documents/schat.py
+++
@@ -1,7 +0,0 @@
-from snapchat import snapchat.def aburney00():
-    doc = "The foo property."
-    def fget(self):
-        return self._foo
-    def fset(self, value):
-        self._foo = value
-        
